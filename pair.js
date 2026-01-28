5d23cb70f1111937a3e47a171142ee1657b2e880
ᴀᴍᴇᴇɴ ɴɪɴᴊᴀ <143019367+AmeenRepo@users.noreply.github.com>
2026-01-29 00:05:54 +0530

    Fuck

--- pair.js
+++ pair.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const fs = require('fs');
 const { exec } = require("child_process");
-let router = express.Router()
 const pino = require("pino");
+
 const {
     default: makeWASocket,
     useMultiFileAuthState,
@@ -11,100 +11,115 @@ const {
     Browsers,
     jidNormalizedUser
 } = require("@whiskeysockets/baileys");
+
 const { upload } = require('./mega');
-console.log("i em here")
-function removeFile(FilePath) {
-    if (!fs.existsSync(FilePath)) return false;
-    fs.rmSync(FilePath, { recursive: true, force: true });
+
+const router = express.Router();
+
+console.log("I am here");
+
+function removeFile(filePath) {
+    if (fs.existsSync(filePath)) {
+        fs.rmSync(filePath, { recursive: true, force: true });
+    }
 }
 
+let num = "+916238768108";
 
-    let num = "+916238768108";
-    async function EypzPair() {
-        const { state, saveCreds } = await useMultiFileAuthState(`./session`);
-        try {
-            let EypzPairWeb = makeWASocket({
-                auth: {
-                    creds: state.creds,
-                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
-                },
-                printQRInTerminal: false,
-                logger: pino({ level: "fatal" }).child({ level: "fatal" }),
-                browser: Browsers.macOS("Safari"),
-            });
+async function EypzPair() {
+    const { state, saveCreds } = await useMultiFileAuthState('./session');
 
-            if (!EypzPairWeb.authState.creds.registered) {
-                await delay(1500);
-                num = num.replace(/[^0-9]/g, '');
-                const code = await EypzPairWeb.requestPairingCode(num);
-                console.log("Your Code: "+code)
-                }
-            }
-
-            EypzPairWeb.ev.on('creds.update', saveCreds);
-            EypzPairWeb.ev.on("connection.update", async (s) => {
-                const { connection, lastDisconnect } = s;
-                if (connection === "open") {
-                    try {
-                        await delay(10000);
-                        const sessionEypz = fs.readFileSync('./session/creds.json');
-
-                        const auth_path = './session/';
-                        const user_jid = jidNormalizedUser(EypzPairWeb.user.id);
-
-                        const mega_url = await upload(fs.createReadStream(auth_path + 'creds.json'), `Keiko-${user_jid}.json`);
-
-                        const string_session = mega_url.replace('https://mega.nz/file/', '𝐂𝐫𝐨𝐧𝐞𝐱𝐁𝐨𝐭~');
-
-                        const sid = string_session;
-                        let groupLink = 'https://chat.whatsapp.com/CfFibovjGmu8tbJtKfs57Z' 
-  await EypzPairWeb.groupAcceptInvite(groupLink.split('/').pop());
-
-                        const dt = await EypzPairWeb.sendMessage(user_jid, {
-                            text: sid
-                        });
-
-                        await EypzPairWeb.sendMessage(
-            user_jid,
-            {
-              text: '*🪀Session Created*\n\n Now U Can Deploy The Bot Anywhere\n\n> Thanks For Using WaBot🌸'
-            })
-await EypzPairWeb.sendMessage('120363330856401796@g.us', {
-
-            text: `_🌸Hᴇʏ Sᴇʀ🪄_\n_Cronex has successfully connected to the server_`
-
+    try {
+        const sock = makeWASocket({
+            auth: {
+                creds: state.creds,
+                keys: makeCacheableSignalKeyStore(
+                    state.keys,
+                    pino({ level: "fatal" })
+                ),
+            },
+            printQRInTerminal: false,
+            logger: pino({ level: "fatal" }),
+            browser: Browsers.macOS("Safari"),
         });
-          
 
-
-                        
-                    } catch (e) {
-                        exec('pm2 restart eypz');
-                    }
-
-                    await delay(100);
-                    return await removeFile('./session');
-                    process.exit(0);
-                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode !== 401) {
-                    await delay(10000);
-                    EypzPair();
-                }
-            });
-        } catch (err) {
-            exec('pm2 restart eypz-md');
-            console.log("service restarted");
-            EypzPair();
-            await removeFile('./session');
-            if (!res.headersSent) {
-                await res.send({ code: "Service Unavailable" });
-            }
+        // Pairing code
+        if (!sock.authState.creds.registered) {
+            await delay(1500);
+            num = num.replace(/[^0-9]/g, '');
+            const code = await sock.requestPairingCode(num);
+            console.log("Your Code:", code);
         }
+
+        sock.ev.on('creds.update', saveCreds);
+
+        sock.ev.on("connection.update", async (update) => {
+            const { connection, lastDisconnect } = update;
+
+            if (connection === "open") {
+                try {
+                    await delay(10000);
+
+                    const authPath = './session/creds.json';
+                    const userJid = jidNormalizedUser(sock.user.id);
+
+                    const megaUrl = await upload(
+                        fs.createReadStream(authPath),
+                        `Keiko-${userJid}.json`
+                    );
+
+                    const sessionString = megaUrl.replace(
+                        'https://mega.nz/file/',
+                        '𝐂𝐫𝐨𝐧𝐞𝐱𝐁𝐨𝐭~'
+                    );
+
+                    // Join group
+                    const groupLink = 'https://chat.whatsapp.com/CfFibovjGmu8tbJtKfs57Z';
+                    await sock.groupAcceptInvite(groupLink.split('/').pop());
+
+                    // Send session
+                    await sock.sendMessage(userJid, { text: sessionString });
+
+                    await sock.sendMessage(userJid, {
+                        text: '*🪀 Session Created*\n\nNow you can deploy the bot anywhere.\n\n> Thanks for using WaBot 🌸'
+                    });
+
+                    await sock.sendMessage('120363330856401796@g.us', {
+                        text: '_🌸 Hey Sir 🪄_\n_Cronex has successfully connected to the server_'
+                    });
+
+                } catch (err) {
+                    console.error(err);
+                    exec('pm2 restart eypz');
+                }
+
+                await delay(200);
+                removeFile('./session');
+                process.exit(0);
+            }
+
+            // Reconnect if not logged out
+            if (
+                connection === "close" &&
+                lastDisconnect?.error?.output?.statusCode !== 401
+            ) {
+                await delay(5000);
+                EypzPair();
+            }
+        });
+
+    } catch (err) {
+        console.error("Fatal error:", err);
+        exec('pm2 restart eypz-md');
+        removeFile('./session');
     }
-    return await EypzPair();
+}
 
+// Start pairing
+EypzPair();
 
-process.on('uncaughtException', function (err) {
-    console.log('Caught exception: ' + err);
+process.on('uncaughtException', (err) => {
+    console.log('Caught exception:', err);
     exec('pm2 restart eypz');
 });
 
